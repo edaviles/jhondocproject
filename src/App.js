86042df94fd94f5d2e2a5c957ac70106d5ede57e
Jhonatan C Hurtado <jhonatan.cortes@servinformacion.com>
2022-03-28 18:12:46 -0500

    Change title

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Deploy React - Azure - Azure Devops</p>
         <a className="App-link" href="https://reactjs.org" target="_blank"> Learn ReactJs </a>
       </header>
     </div>
