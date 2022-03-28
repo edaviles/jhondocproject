251a613892d3c0584013dfaee37213064bb6eaa6
Jhonatan C Hurtado <jhonatan.cortes@servinformacion.com>
2022-03-28 18:16:24 -0500

    noopener

--- src/App.js
+++ src/App.js
@@ -6,8 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Deploy React - Azure - Azure Devops</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank"> Learn ReactJs </a>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
